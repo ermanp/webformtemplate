8e71605713ec2775a17ff66f27ed783f27b71a2a
Erman Payasli <ermanpayasli@gmail.com>
2018-06-16 16:02:04 +0300

    First commit

---
+++ src/main/java/com/webformtemplate/WebformtemplateApplication.java
@@ -0,0 +1,12 @@
+package com.webformtemplate;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class WebformtemplateApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(WebformtemplateApplication.class, args);
+	}
+}
